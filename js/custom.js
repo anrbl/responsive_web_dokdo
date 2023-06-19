99490476cc8c07fc1f31da3110935072a63871c6
anrbl <133613567+anrbl@users.noreply.github.com>
2023-06-19 14:15:34 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -38,9 +38,17 @@ $(function () {
 
 
     const noticeSlide = new Swiper('.notice_slide', {
-        slidesPerView: 2,
-        spaceBetween: 30,
+        slidesPerView: 1,
+        spaceBetween: 0,
         loop: true,
+
+        breakpoints: {
+            // when window width is >= 768px
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+        },
     });
     $('.main_notice .arrows .right').on('click', function () {
         noticeSlide.slideNext();
@@ -50,4 +58,31 @@ $(function () {
     });
 
 
+
+    //media 768
+    $('.mobile_btn').on('click', function () {
+        $('.gnb').toggleClass('on');
+        $('.header').toggleClass('mobile_on');
+    });
+
+    $('.gnb .main_menu>li>a').on('click', function (e) {
+        //width를 잡기보다는 .gnb hasClass on으로 찾는게 좋다
+        if ($('.gnb').hasClass('on')) {
+            e.preventDefault();
+            $(this).next().stop().slideToggle();
+            $(this).parent().siblings().find('.sub_menu').stop().slideUp();
+            //나의 부모의 > 형제들(삼촌고모)를 찾는다 > stop+slideUp (삼촌들 치워버림)
+        }
+
+    });
+    //사이즈 조절될때 gnb에 on을 뺀다
+    $(window).on('resize', function () {
+        $('.gnb').removeClass('on');
+    })
+    //휠을 써도 움직이지 않게 고정시키는 함수
+    $('.gnb').on('wheel', function (e) {
+        if ($(this).hasClass('on')) {
+            e.preventDefault();
+        }
+    })
 });
